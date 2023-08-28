b8d3dd176165c5513e8fa74baffd8f93614ab55a
Elena Popova <ep4360619@gmail.com>
2023-08-28 14:53:41 +0300

    стилизует каталог

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,6 +1,9 @@
 let mobileNavigation = document.querySelector('.main-nav');
 let navButton = document.querySelector('.main-nav__toggle');
 
+document.querySelector('.main-nav__list').classList.add('main-nav__list--js');
+document.querySelector('.main-nav').classList.add('main-nav--closed');
+
 navButton.onclick = function() {
   mobileNavigation.classList.toggle('main-nav--closed');
 };
