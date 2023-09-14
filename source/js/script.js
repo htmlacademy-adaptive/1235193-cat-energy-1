6617a9a3f243f7083d34c5ef5eda2e75e3765080
Elena Popova <ep4360619@gmail.com>
2023-09-14 23:26:17 +0300

    финальные правки перед 2 попыткой

--- source/js/script.js
+++ source/js/script.js
@@ -3,18 +3,20 @@ const navButton = document.querySelector('.main-nav__toggle');
 const sliderButton = document.querySelector('.slider__button');
 const slider = document.querySelector('.slider');
 
-document.querySelector('.main-nav__list').classList.add('main-nav__list--js');
+document.querySelector('.main-nav').classList.add('main-nav--js');
 document.querySelector('.main-nav').classList.add('main-nav--closed');
 
-navButton.onclick = function() {
+navButton.onclick = function () {
   mobileNavigation.classList.toggle('main-nav--closed');
 };
 
-sliderButton.addEventListener('click', () => {
-  if(!document.querySelector('.slider--after') && !document.querySelector('.slider--before')) {
-  slider.classList.add('slider--after');
-}else {
-  slider.classList.toggle('slider--after')
-  slider.classList.toggle('slider--before');
-  }
-});
+if (slider) {
+  sliderButton.addEventListener('click', () => {
+    if (!document.querySelector('.slider--after') && !document.querySelector('.slider--before')) {
+      slider.classList.add('slider--after');
+    } else {
+      slider.classList.toggle('slider--after')
+      slider.classList.toggle('slider--before');
+    }
+  });
+}
